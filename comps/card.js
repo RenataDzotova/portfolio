f914a3f11323d8cf8656e4b8ba9bdc8a4266b870
RenataDzotova <rdzotova@my.bcit.ca>
2023-03-31 22:00:22 -0700

    style

--- comps/card.js
+++ comps/card.js
@@ -9,14 +9,14 @@ export default function Card({
     description='description'
 }){
     return(
-        <FlexBox margin="35px" width="770px" height="fit-content" fontFamily="PT Sans" bgColor="#F2F2F2" color="#121212" boxShadow="4px 4px 15px 3px rgba(0, 0, 0, 0.15)" borderRadius="15px">
-          <Image src={img} width="380px" height="335px" borderRadius="15px 0 0 15px"></Image>
-          <FlexBox fontFamily="PT Sans" dir="column" alignItems="start" justifyContent="space-around" width="100%" padding="20px 35px" height="335px">
-            <Text fontSize="43px">{name}</Text>
-            <Text color="#6C6C6C" fontSize="19px" margin="5px 0 0 0">{role}</Text>
-            <Text lineHeight="150%" fontSize="19px" margin="10px 0 0 0">{description}</Text>
+        <FlexBox margin="35px" width="725px" height="fit-content" fontFamily="PT Sans" bgColor="#F2F2F2" color="#121212" boxShadow="4px 4px 15px 3px rgba(0, 0, 0, 0.15)" borderRadius="15px">
+          <Image src={img} width="330px" height="305px" borderRadius="15px 0 0 15px"></Image>
+          <FlexBox fontFamily="PT Sans" dir="column" alignItems="start" justifyContent="space-around" width="100%" padding="20px 35px" height="305px">
+            <Text fontSize="37px" color='#222222'>{name}</Text>
+            <Text color="#6C6C6C" fontSize="18px" margin="5px 0 0 0">{role}</Text>
+            <Text lineHeight="150%" fontSize="17px" margin="10px 0 0 0" color='#222222'>{description}</Text>
           <FlexBox bgColor="#252525" padding="8px 17px 8px 17px" borderRadius="8px" margin="30px 0 0 0">
-            <Text fontSize="20px" color="white">View Study Case</Text>
+            <Text fontSize="17px" color="white">View Study Case</Text>
           </FlexBox>
           </FlexBox>
         </FlexBox>
--- comps/navbar.js
+++ comps/navbar.js
@@ -13,7 +13,7 @@ export default function NavBar({
       const handleScroll = () => {
           const scrollPos = window.pageYOffset;
           if (scrollPos > 0) {
-              setBgColor("rgba(113, 122, 221, 1)");
+              setBgColor("rgba(113, 122, 221, 0.5)");
           } else {
               setBgColor("rgba(113, 122, 221, 0)");
           }
@@ -25,7 +25,7 @@ export default function NavBar({
   }, []);
 
     return(
-        <FlexBox fontFamily="PT Sans Narrow" position="fixed" alignItems="end" bgColor={bgColor} top="0" width="100vw" height="60px" justifyContent="space-between" padding="10px 50px 10px 50px" zIndex="2">
+        <FlexBox fontFamily="PT Sans Narrow" position="fixed" alignItems="end" bgColor={bgColor} top="10px" width="90vw" height="60px" justifyContent="space-between" padding="10px 50px 10px 50px" zIndex="2" borderRadius='10px'>
           <FlexBox width="50%" justifyContent="flex-start">
           <Image className="logoMobile" src="/logo.svg" width="30px"></Image>
           </FlexBox>
